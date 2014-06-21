b780687c1ee24ea72e13a88fca4ae14bf54ff56c
brandbir <brandbir@gmail.com>
2014-06-21 11:01:23 +0200

    Updating server to send file contents over TCP/IP to the client Added a sample file that will be mapped on the client side's memory

--- Systems Programming/server/src/server.c
+++ Systems Programming/server/src/server.c
@@ -2,67 +2,88 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
 
 int main(int argc, char *argv[])
 {
-	// comments
 	// Declare variables
-	int sockfd, newsockfd, portno, clilen;
-	char buffer[256];
-	struct sockaddr_in serv_addr, cli_addr;
-	int n;
+	int listen_fd, client_conn;
+	struct sockaddr_in serv_addr;
 
-	// Create server socket (AF_INET, SOCK_STREAM)
-	sockfd = socket(AF_INET, SOCK_STREAM, 0);
-	if (sockfd < 0) //fgfgfgfgf
+	listen_fd = socket(AF_INET, SOCK_STREAM, 0);
+
+	if (listen_fd < 0)
 	{
 		perror("ERROR opening socket");
 		exit(1);
 	}
 
-	// Initialize socket structure
+	// Initialise socket structure
 	bzero((char *) &serv_addr, sizeof(serv_addr));
-	portno = 5001;  // Server port
 	serv_addr.sin_family = AF_INET;
 	serv_addr.sin_addr.s_addr = INADDR_ANY; // Accept connections from any address
-	serv_addr.sin_port = htons(portno);
+	serv_addr.sin_port = htons(5001);
 
 	// Bind the host address
-	if (bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) {
+	if (bind(listen_fd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0)
+	{
 		perror("ERROR on binding");
 		exit(1);
 	}
 
 	// Start listening for the clients, here process will
 	// go in sleep mode and will wait for the incoming connection
-	listen(sockfd, 5);
-	clilen = sizeof(cli_addr);
+	listen(listen_fd, 5);
 
-	// Accept actual connection from the client
-	newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
-	if (newsockfd < 0) {
-		perror("ERROR on accept");
-		exit(1);
+	while(1)
+	{
+		printf("%s \n", "Waiting for connections...");
+		//Accepting client connection
+		client_conn = accept(listen_fd, (struct sockaddr *) NULL, NULL);
+		if (client_conn < 0)
+		{
+			perror("ERROR on accept");
+			exit(1);
+		}
+
+		//Opening the file located on the server
+		FILE *file = fopen("file.txt", "rb");
+		if(file == NULL)
+		{
+			printf("File cannot be opened");
+			return 1;
+		}
+
+		while(1)
+		{
+			char buffer[256];
+			bzero(buffer, 256);
+			int bytes_read = fread(buffer, 1, 256, file);
+			printf("Number of bytes read %d \n", bytes_read);
+
+			if(bytes_read > 0)
+			{
+				printf("Sending data to the client\n");
+				write(client_conn, buffer, bytes_read);
+			}
+
+			if(bytes_read < 256)
+			{
+				if(feof(file))
+					printf("End of file\n");
+
+				if(ferror(file))
+					printf("Read Error\n");
+
+				break;
+			}
+		}
+
+		//Closing client connection
+		close(client_conn);
 	}
 
-	// If connection is established then start communicating
-	bzero(buffer, 256);
-	n = read(newsockfd, buffer, 255);
-	if (n < 0) {
-		perror("ERROR reading from socket");
-		exit(1);
-	}
-	printf("Here is the message: %s\n", buffer);
-
-	// Write a response to the client
-	n = write(newsockfd, "I got your message", 18);
-	if (n < 0) {
-		perror("ERROR writing to socket");
-		exit(1);
-	}
-
-	// All done, close sockets
-	close(newsockfd);
-	close(sockfd);
 	return 0;
 }
